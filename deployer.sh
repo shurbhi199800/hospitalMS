781d503aafc52208e0c345e2b5f669a1179fece4
shurbhi199800 <154580577+shurbhi199800@users.noreply.github.com>
2023-12-25 10:39:40 +0530

    Update deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -1,1 +1,10 @@
 
+set -e
+
+echo "Application deploying"
+
+(php artisan down --message 'this app is being quickly updated please try again later in a minute')
+    git pull origin main
+php artisan up    
+
+echo "Application deployed"
